6469881f7a4cc5acf941c768400969ea61393209
Damara Bachtiar <67513180+Damarabc@users.noreply.github.com>
2020-11-19 20:51:43 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,23 @@
+<?php
+
+    $mahasiswa = 
+    [
+
+        "nama" => "Damara Bachtiar",
+        "nrp" => "183040135",
+        "email" => "bachtiar.183040135@mail.unpas.ac.id",
+
+    
+
+        "nama" => "Erik Doang",
+        "nrp" => "023040001",
+        "email" => "erik@gmail.com"
+
+    
+   
+    ];
+
+    $data = json_encode($mahasiswa);
+    echo $data;
+
+?>
---
+++ script.js
@@ -0,0 +1,7 @@
+let mahasiswa = {
+    nama: "Sandhika Galih",
+    nrp: "03040023",
+    email: "sandhikagalih@unpas.ac.id"
+}
+
+console.log(JSON.stringify(mahasiswa));
